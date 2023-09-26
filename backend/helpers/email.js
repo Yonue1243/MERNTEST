a76f5c5806d1b110ec6a9aa70fcbb29e78361120
Facundo Rodriguez <rodriguezdelfacundo@gmail.com>
2023-09-25 21:15:11 -0300

    Frontend progress

--- backend/helpers/email.js
+++ backend/helpers/email.js
@@ -4,11 +4,11 @@ export const emailRegistro = async (datos) => {
   const { email, nombre, token } = datos;
 
   const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
-      user: "800e2634034a97",
-      pass: "cfe5d5d69ed573"
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
     }
   });
 
